2330847c244094040de4e01395167fb78372f692
Ryan Southard <eodchop@gmail.com>
2018-04-11 10:03:13 -0400

    added support for setting vm type in the scs resource config

--- tasks/config-spring-cloud/task.sh
+++ tasks/config-spring-cloud/task.sh
@@ -146,3 +146,27 @@ else
   fi
 fi
 
+#stuffs for setting vm type to accomodate increased memory utilization in errands from runtime config deployments
+if [[ -z "$SCS_BROKER_DEPLOYER_TYPE" ]]; then
+   scs_broker_deployer_type="automatic"
+else
+   scs_broker_deployer_type="$SCS_BROKER_DEPLOYER_TYPE"
+fi
+
+if [[ -z "$SCS_BROKER_REGISTRAR_TYPE" ]]; then
+   scs_broker_registrar_type="automatic"
+else
+   scs_broker_registrar_type="$SCS_BROKER_REGISTRAR_TYPE"
+fi
+
+if [[ -z "$SCS_SMOKE_TEST_TYPE" ]]; then
+   scs_smoke_test_type="automatic"
+else
+   scs_smoke_test_type="$SCS_SMOKE_TEST_TYPE"
+fi
+
+if [[ -z "$SCS_BROKER_DEREGISTRAR_TYPE" ]]; then
+   scs_broker_deregistrar_type="automatic"
+else
+   scs_broker_deregistrar_type="$SCS_BROKER_DEREGISTRAR_TYPE"
+fi
