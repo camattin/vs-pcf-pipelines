74a41cbbde68979a9218f9f7463ea5c01b64c290
Ryan Southard <eodchop@gmail.com>
2018-04-11 21:39:34 -0400

    fixed variable to match input from secrets.yml

--- tasks/config-spring-cloud/task.sh
+++ tasks/config-spring-cloud/task.sh
@@ -17,30 +17,23 @@ function fn_other_azs {
 }
 
 #stuffs for setting vm type to accomodate increased memory utilization in errands from runtime config deployments
-if [[ -z "$SCS_BROKER_DEPLOYER_TYPE" ]]; then
-   scs_broker_deployer_type="automatic"
-else
-   scs_broker_deployer_type="$SCS_BROKER_DEPLOYER_TYPE"
-fi
-
-if [[ -z "$SCS_BROKER_REGISTRAR_TYPE" ]]; then
-   scs_broker_registrar_type="automatic"
-else
-   scs_broker_registrar_type="$SCS_BROKER_REGISTRAR_TYPE"
-fi
-
-if [[ -z "$SCS_SMOKE_TEST_TYPE" ]]; then
-   scs_smoke_test_type="automatic"
-else
-   scs_smoke_test_type="$SCS_SMOKE_TEST_TYPE"
-fi
-
-if [[ -z "$SCS_BROKER_DEREGISTRAR_TYPE" ]]; then
-   scs_broker_deregistrar_type="automatic"
-else
-   scs_broker_deregistrar_type="$SCS_BROKER_DEREGISTRAR_TYPE"
-fi
-####END of Stuffs for setting resource vm type########
+RESOURCES=$(cat <<-EOF
+{
+  "deploy-service-broker": { 
+    "instance_type": {"id": "$scs_broker_deployer_type"}
+  },
+  "bregister-service-broker": {
+    "instance_type": {"id": "$scs_broker_registrar_type"}
+  },
+  "run-smoke-tests": {
+    "instance_type": {"id": "$scs_smoke_test_type"}
+  },
+  "destroy-service-broker": {
+    "instance_type": {"id": "$scs_broker_deregistrar_type"}
+  }
+}
+EOF
+)
 
 OTHER_AZS=$(fn_other_azs $OTHER_JOB_AZS)
 
@@ -82,6 +75,8 @@ RESOURCES=$(cat <<-EOF
 }
 EOF
 )
+####END of Stuffs for setting resource vm type########
+
 $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$PROPERTIES" -pn "$NETWORK" -pr "$RESOURCES"
 
 if [[ -z "$ERRANDS_TO_DISABLE" ]] || [[ "$ERRANDS_TO_DISABLE" == "none" ]]; then
