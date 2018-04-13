46f84e463512312546a3bd77af32b73852c92cce
Ryan Southard <eodchop@gmail.com>
2018-04-13 16:20:47 -0400

    missing bin bash line

--- tasks/config-spring-cloud/task.sh
+++ tasks/config-spring-cloud/task.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+-
 #mv tool-om/om-linux-* tool-om/om-linux
 chmod +x tool-om/om-linux
 CMD=./tool-om/om-linux
@@ -75,8 +77,6 @@ PROPERTIES=$(cat <<-EOF
 }
 EOF
 )
-
-####END of Stuffs for setting resource vm type########
 
 $CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$PROPERTIES" -pn "$NETWORK" -pr "$RESOURCES"
 
